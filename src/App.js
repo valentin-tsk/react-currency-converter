2ebcdb52226d76b6ea83f044290519f0b6d33bf1
Valentin Krakovetskiy <valek.krak@gmail.com>
2023-03-24 20:33:38 +0700

    added table with other currencies ratio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Converter from './components/Converter'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
--- src/components/Converter.js
+++ src/components/Converter.js
@@ -5,89 +5,114 @@ import {HiSwitchHorizontal} from 'react-icons/hi';
 import './Converter.css';
 
 const Converter = () => {
-    const [apiUrl] = useState('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies')
-    const [currencies, setCurrencies] = useState([]);
+        const [apiUrl] = useState('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies');
+        const [currencies, setCurrencies] = useState([]);
 
-    const [from, setFrom] = useState('usd');
-    const [to, setTo] = useState('rub');
+        const [from, setFrom] = useState('usd');
+        const [to, setTo] = useState('rub');
 
-    const [amount, setAmount] = useState(0);
-    const [options, setOptions] = useState([]);
-    const [output, setOutput] = useState(0);
+        const [amount, setAmount] = useState(0);
+        const [options, setOptions] = useState([]);
+        const [output, setOutput] = useState(0);
 
-    useEffect(() => {
-        Axios.get(
-            `${apiUrl}/${from}.json`)
-            .then((res) => {
-                setCurrencies(res.data[from]);
-            })
-    }, [from, to]);
+        useEffect(() => {
+            Axios.get(
+                `${apiUrl}/${from}.json`)
+                .then((res) => {
+                    console.log(res.data[from]);
+                    setCurrencies(res.data[from]);
+                })
+        }, [from, to]);
 
-    // Calling convert when user switches the currency
-    useEffect(() => {
-        setOptions(Object.keys(currencies).map((item) => {
-            return {
-                value: item,
-                label: item
-            }
-        }));
-        convert();
-    }, [currencies, amount])
+        useEffect(() => {
+            setOptions(Object.keys(currencies).map((item) => {
+                return {
+                    value: item,
+                    label: item
+                }
+            }));
+            convert();
+        }, [currencies, amount])
 
-    // Function to convert the currency
-    function convert() {
-        var rate = currencies[to];
-        setOutput(amount * rate);
+        function convert() {
+            var rate = currencies[to];
+            setOutput(amount * rate);
+        }
+
+        function flip() {
+            var temp = from;
+            setFrom(to);
+            setTo(temp);
+        }
+
+        return (
+            <>
+                <div className="container">
+                    <div className="left">
+                        <h3>Amount</h3>
+                        <input type="text"
+                               placeholder="Enter the amount"
+                               onChange={(e) => {
+                                   setAmount(e.target.value);
+                               }}/>
+                    </div>
+                    <div className="middle">
+                        <h3>From</h3>
+                        <Select options={options}
+                                isSearchable={true}
+                                onChange={(e) => {
+                                    setFrom(e.value)
+                                }}
+                                value={from.value} placeholder={from}/>
+                    </div>
+                    <div className="switch">
+                        <HiSwitchHorizontal size="30px"
+                                            onClick={() => {
+                                                flip()
+                                            }}/>
+                    </div>
+                    <div className="right">
+                        <h3>To</h3>
+                        <Select options={options}
+                                isSearchable={true}
+                                onChange={(e) => {
+                                    setTo(e.value)
+                                }}
+                                value={to.value} placeholder={to}/>
+                    </div>
+                </div>
+                <div className="result">
+                    <p>{amount + " " + from + " = " + output.toFixed(2) + " " + to}</p>
+                </div>
+                <div className="ratio-table">
+                    <table className="table">
+                        <thead>
+                        <th>
+                            Code
+                        </th>
+                        <th>
+                            Ratio from {from}
+                        </th>
+                        </thead>
+                        <tbody>
+                        {Object.keys(currencies).map((currencyCode) => {
+                            return (
+                                <tr>
+                                    <td>
+                                        {currencyCode}
+                                    </td>
+                                    <td>
+                                        {currencies[currencyCode]}
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                        </tbody>
+                    </table>
+                </div>
+            </>
+        )
     }
-
-// Function to switch between two currency
-    function flip() {
-        var temp = from;
-        setFrom(to);
-        setTo(temp);
-    }
-
-    return (
-        <>
-            <div className="container">
-                <div className="left">
-                    <h3>Amount</h3>
-                    <input type="text"
-                           placeholder="Enter the amount"
-                           onChange={(e) => {
-                               setAmount(e.target.value);
-                           }}/>
-                </div>
-                <div className="middle">
-                    <h3>From</h3>
-                    <Select options={options}
-                            isSearchable={true}
-                            onChange={(e) => {
-                                setFrom(e.value)
-                            }}
-                            value={from.value} placeholder="From"/>
-                </div>
-                <div className="switch">
-                    <HiSwitchHorizontal size="30px"
-                                        onClick={() => {
-                                            flip()
-                                        }}/>
-                </div>
-                <div className="right">
-                    <h3>To</h3>
-                    <Select options={options}
-                            isSearchable={true}
-                            onChange={(e) => {
-                                setTo(e.value)
-                            }}
-                            value={to.value} placeholder="To"/>
-                </div>
-            </div>
-            <div className="result">
-                <p>{amount + " " + from + " = " + output.toFixed(2) + " " + to}</p>
-            </div>
-        </>
-    )
-};
+;
 
 export default Converter;
